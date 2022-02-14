04bc3173749a65b59424b4e309c2121fa248041b
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-14 19:37:42 +0100

    Update example usersettings gamble part

--- tests/test_parameter_handler/usersettings_example_gamble_task.py
+++ tests/test_parameter_handler/usersettings_example_gamble_task.py
@@ -1,4 +1,4 @@
-task = "gamble"
+TASK = "gamble"
 
 GAMBLE_SIDE = "Left"
 BLOCKS = [
@@ -7,7 +7,7 @@ BLOCKS = [
     {"trial_range_block": [5, 6], "prob_reward_gamble_block": 50.0, "prob_reward_save_block": 50.0},
 ]
 
-# reward in ml
+# reward in seconds
 BIG_REWARD = 0.11
 SMALL_REWARD = 0.12
 LAST_CALLIBRATION = "2020.06.10"
@@ -16,22 +16,23 @@ LAST_CALLIBRATION = "2020.06.10"
 TIME_START = 1.0
 TIME_WHEEL_STOPPING_CHECK = 1.0
 TIME_WHEEL_STOPPING_PUNISH = 0.0
-TIME_PRESENT_STIM = 1.0
+TIME_PRESENT_STIMULUS = 1.0
 TIME_OPEN_LOOP = 10.0
 TIME_OPEN_LOOP_FAIL_PUNISH = 0.0
-TIME_STIM_FREEZ = 2.0
-REWARD_TIME = 1.0
-NOREWARD_TIME = 1.0
-INTER_TRIAL_TIME = 1.5
+TIME_STIMULUS_FREEZE = 2.0
+TIME_REWARD = 1.0
+TIME_NO_REWARD = 1.0
+TIME_INTER_TRIAL = 1.5
+
 # stimulus size and color - only for moving stimulus
 STIMULUS_RAD = 45  # pixel radius of stimulus
 STIMULUS_COL = [0, 255, 0]  # color of stimulus
-BACKGROUND_COL = [0, 0, 0]  # -1,-1,-1 for black
+BACKGROUND_COL = [0, 0, 0]
 
-# threhsolds
-ALL_THRESHOLDS = [-90, 90, -2, 2]
-STIM_END_POS = [-2048, 2048]  # pixel
+# thresholds
+ROTARYENCODER_THRESHOLDS = [-90, 90, -2, 2]
+STIMULUS_END_POS = [-2048, 2048]  # pixel
 
 LIFE_PLOT = False
-# animal waight in grams
-ANIMAL_WAIGHT = 10.0
+# animal weight in grams
+ANIMAL_WEIGHT = 10.0
