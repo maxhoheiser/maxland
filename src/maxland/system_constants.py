4fe586b33cbfe070fbfde6b5d9724ebc7219a09f
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-14 18:48:42 +0100

    Add external file for system constants

---
+++ src/maxland/system_constants.py
@@ -0,0 +1,21 @@
+# system settings for each session
+SOFT_CODE_PRESENT_STIMULUS = 1
+SOFT_CODE_START_OPEN_LOOP = 2
+SOFT_CODE_STOP_OPEN_LOOP = 3
+SOFT_CODE_END_PRESENT_STIMULUS = 4
+SOFT_CODE_START_LOGGING = 5
+SOFT_CODE_END_LOGGING = 6
+SOFT_CODE_STOP_CLOSE_LOOP = 7
+# rotary encoder
+WHEEL_DIAMETER = 6.3
+RESET_ROTARY_ENCODER = 1
+THRESH_LEFT = "RotaryEncoder1_4"
+THRESH_RIGHT = "RotaryEncoder1_3"
+STIMULUS_LEFT = "RotaryEncoder1_2"
+STIMULUS_RIGHT = "RotaryEncoder1_1"
+# stimulus
+FPS = 60
+SCREEN_WIDTH = 6144
+SCREEN_HEIGHT = 1536
+MONITOR_DISTANCE = 16  # Distance between subject's eyes and monitor
+MONITOR_WIDTH = 60  # Width of your monitor in cm
