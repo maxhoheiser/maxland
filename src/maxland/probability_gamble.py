e6510448f5aeef5e6879086bf39aa527fb10e08f
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-15 23:38:05 +0100

    Refactor probability_gamble

--- src/maxland/probability_gamble.py
+++ src/maxland/probability_gamble.py
@@ -1,88 +1,72 @@
 import random
 
 
-class ProbabilityConstuctor:
+class ProbabilityConstructor:
+    """
+    Module to calculate and handle probability for all events with given usersettings in session e.g. reward left and reward right probability
+    Args:
+        settings (TrialParameterHandler object): the object for all the session parameters from TrialParameterHandler
+    """
+
     def __init__(self, settings):
-        """object to calculate and handle probability for all events with given usersettings in session e.g. reward left and reward right probability
-
-        Args:
-            settings (TrialParameterHandler object): the object for all the session parameters from TrialPArameterHandler
-        """
         self.settings = settings
-        self.trial_num = self.block_trial_builder("trial_range_block")
-        self.probability_list = []
-        # define block number for block dict
-        block_num = 0
-        for block in self.settings.blocks:
-            self.probability_list.extend(self.block_list_builder(block, self.settings.gamble_side_left, block_num))
-            block_num += 1
+        self.set_block_trial_number_from_range()
+        self.trial_number = self.get_total_trial_count()
+        self.probability_list = self.get_probability_list()
         settings.probability_list = self.probability_list
-        settings.trial_num = self.trial_num
+        settings.trial_number = self.trial_number
 
-    def block_trial_builder(self, trial_range_block):
-        """randomly choose block trial length from range and add to block dict
-
-        Args:
-            trial_range_block (list): list of integers given the trial range for given block
-
-        Returns:
-            int: probabalistically derived trial number in given trial range
-        """
-        trial_num = 0
+    def set_block_trial_number_from_range(self):
         for block in self.settings.blocks:
-            block_trial_range = block[trial_range_block]
-            block_trial_num = random.randint(block_trial_range[0], block_trial_range[1])
-            # here the value of settings object block variable are changed !
-            block["block_trial_num"] = block_trial_num
-            trial_num += block_trial_num
-        return trial_num
+            block_trial_range = block["trial_range_block"]
+            block_trial_number = random.randint(block_trial_range[0], block_trial_range[1])
+            block["block_trial_number"] = block_trial_number
 
-    def probability_builder(self, probability, trial_num):
-        """generate a random shuffeled list of defined probability of booleans
+    def get_total_trial_count(self):
+        trial_number = 0
+        for block in self.settings.blocks:
+            trial_number += block["block_trial_number"]
+        return trial_number
 
-        Args:
-            probability (float): probability in percentage (0.9 = 90%) for gamble (big) reward
-            trial_num (int): number of trials in this block
+    def get_probability_list(self):
+        probability_list = []
+        block_number = 0
+        for block in self.settings.blocks:
+            probability_list.extend(
+                self.get_all_trials_of_blocks(block, self.settings.is_gamble_side_left, block_number)
+            )
+            block_number += 1
+        return probability_list
 
-        Returns:
-            list_rand (list): list of boolean values with gamble probability distribution
-        """
-        list_rand = []
-        number_true = int(trial_num * probability)
-        number_false = int(trial_num * (1 - probability))
-        for _ in range(trial_num):
-            if _ < number_true:
-                list_rand.append(True)
-            elif _ < (number_false + number_true):
-                list_rand.append(False)
-        if len(list_rand) < trial_num:
-            list_rand.append(bool(random.getrandbits(1)))
-        random.shuffle(list_rand)
-        return list_rand
+    def get_rewarded_not_rewarded_trials(self, probability, trial_number):
+        rewarded_not_rewarded_trials = []
+        total_trials_rewarded = int(trial_number * probability)
+        total_trials_not_rewarded = int(trial_number * (1 - probability))
+        for _ in range(trial_number):
+            if _ < total_trials_rewarded:
+                rewarded_not_rewarded_trials.append(True)
+            elif _ < (total_trials_not_rewarded + total_trials_rewarded):
+                rewarded_not_rewarded_trials.append(False)
+        if len(rewarded_not_rewarded_trials) < trial_number:
+            rewarded_not_rewarded_trials.append(bool(random.getrandbits(1)))
+        random.shuffle(rewarded_not_rewarded_trials)
+        return rewarded_not_rewarded_trials
 
-    def block_list_builder(self, block, gamble_left, block_num):
-        """create a dictionary for each trial in given block with both save and gamble reward based on given probability in settings
-
-        Args:
-            block (dict): [description]
-            gamble_left (bool): [description]
-            block_num (int): number of current block
-
-        Returns:
-            [type]: [description]
-        """
-        gamble_reward_list = self.probability_builder(
-            (block["prob_reward_gamble_block"] / 100), block["block_trial_num"]
+    def get_all_trials_of_blocks(self, block, is_gamble_left, block_number):
+        gamble_reward_list = self.get_rewarded_not_rewarded_trials(
+            (block["prob_reward_gamble_block"] / 100), block["block_trial_number"]
         )
-        save_reward_list = self.probability_builder((block["prob_reward_save_block"] / 100), block["block_trial_num"])
-        block_list = []
-        for trial in range(block["block_trial_num"]):
+        save_reward_list = self.get_rewarded_not_rewarded_trials(
+            (block["prob_reward_save_block"] / 100), block["block_trial_number"]
+        )
+        all_trials_of_block = []
+        for trial in range(block["block_trial_number"]):
             trial_dict = {
-                "gamble_left": gamble_left,
+                "gamble_left": is_gamble_left,
                 "gamble_reward": gamble_reward_list[trial],
                 "safe_reward": save_reward_list[trial],
-                "block": block_num,
+                "block": block_number,
             }
-            block_list.append(trial_dict)
-        block["block_rewards"] = block_list
-        return block_list
+            all_trials_of_block.append(trial_dict)
+        block["block_rewards"] = all_trials_of_block
+        return all_trials_of_block
