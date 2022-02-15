c5d9f696e141e225323c98cb241068b4c479455d
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-15 11:23:47 +0100

    Add test for probability conf module

---
+++ tests/test_probability_confidentiality_task.py
@@ -0,0 +1,97 @@
+import random
+import unittest
+from unittest.mock import patch
+
+from maxland.parameter_handler import TrialParameterHandler
+from maxland.probability_conf import ProbabilityConstructor
+
+
+class Dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+USERSETTINGS = Dotdict(
+    {
+        "TASK": "conf",
+        "TRIAL_NUMBER": 30,
+        "STIMULUS_TYPE": "two-stimuli",
+        "STIMULUS_CORRECT": {"grating_sf": 0.01, "grating_ori": 0.1, "grating_size": 40.0, "grating_speed": 0.04},
+        "STIMULUS_WRONG": {"grating_sf": 0.04, "grating_ori": 90.0, "grating_size": 40.0, "grating_speed": 0.01},
+        "REWARD": 0.12,
+        "LAST_CALLIBRATION": "2020.06.10",
+        "TIME_START": 1.0,
+        "TIME_WHEEL_STOPPING_CHECK": 1.0,
+        "TIME_WHEEL_STOPPING_PUNISH": 0.0,
+        "TIME_PRESENT_STIMULUS": 1.0,
+        "TIME_OPEN_LOOP": 10.0,
+        "TIME_OPEN_LOOP_FAIL_PUNISH": 0.0,
+        "TIME_STIMULUS_FREEZE": 2.0,
+        "TIME_REWARD": 1.0,
+        "TIME_RANGE_OPEN_LOOP_WRONG_PUNISH": [0.0, 0.0],
+        "TIME_INTER_TRIAL": 1.5,
+        "INSIST_RANGE_TRIGGER": 5,
+        "INSIST_CORRECT_DEACTIVATE": 3,
+        "INSIST_RANGE_DEACTIVATE": 35,
+        "RULE_SWITCH_INITIAL_WAIT": 10,
+        "RULE_SWITCH_RANGE": 10,
+        "RULE_SWITCH_CORRECT": 8,
+        "FADE_START": 1950,
+        "FADE_END": 3000,
+        "STIMULUS_RAD": 45,
+        "STIMULUS_COL": [0, 255, 0],
+        "BACKGROUND_COL": [0, 0, 0],
+        "ROTARYENCODER_THRESHOLDS": [-90, 90, -2, 2],
+        "STIMULUS_END_POS": [-2048, 2048],
+        "LIFE_PLOT": False,
+        "ANIMAL_WEIGHT": 10.0,
+    }
+)
+
+
+class TestProbabilityConstructorConfidentialityTask(unittest.TestCase):
+    def setUp(self):
+        self.parameter_handler = TrialParameterHandler(USERSETTINGS, "", "")
+        random.seed(666)
+        self.random = random
+
+    def tearDown(self):
+        self.parameter_handler = None
+
+    def test_load_probability_constructor_module(self):
+        ProbabilityConstructor(self.parameter_handler)
+
+    @patch("maxland.probability_conf.random")
+    def test_get_random_side(self, random):
+        random.getrandbits._mock_side_effect = self.random.getrandbits
+        probability_constructor = ProbabilityConstructor(self.parameter_handler)
+        probability_constructor.get_random_side()
+
+        self.assertEqual(probability_constructor.stimulus_sides["right"], False)
+        self.assertEqual(probability_constructor.stimulus_sides["left"], True)
+
+    @patch("maxland.probability_conf.random")
+    def test_ret_random_side_insist_mode_active_right(self, random):
+        random.getrandbits._mock_side_effect = self.random.getrandbits
+        probability_constructor = ProbabilityConstructor(self.parameter_handler)
+        probability_constructor.insist_mode_active = True
+        probability_constructor.insist_side = "right"
+        probability_constructor.get_random_side()
+
+        self.assertEqual(probability_constructor.stimulus_sides["right"], True)
+        self.assertEqual(probability_constructor.stimulus_sides["left"], False)
+
+    @patch("maxland.probability_conf.random")
+    def test_ret_random_side_insist_mode_active_left(self, random):
+        random.seed(555)
+        random.getrandbits._mock_side_effect = random.getrandbits
+        probability_constructor = ProbabilityConstructor(self.parameter_handler)
+        probability_constructor.insist_mode_active = True
+        probability_constructor.insist_side = "left"
+        probability_constructor.get_random_side()
+
+        self.assertEqual(probability_constructor.stimulus_sides["right"], False)
+        self.assertEqual(probability_constructor.stimulus_sides["left"], True)
