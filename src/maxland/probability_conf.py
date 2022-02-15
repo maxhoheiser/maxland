f5eaa7789cafdb3d8a75dc8b1537568caa782396
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-15 22:53:51 +0100

    Refactor probability conf module

--- src/maxland/probability_conf.py
+++ src/maxland/probability_conf.py
@@ -69,57 +69,53 @@ class ProbabilityConstructor:
         # check for insist mode activate
         if not self.insist_mode_active:
             if len(self.chosen_sides_li) >= self.settings.insist_range_trigger:
-                slice = self.chosen_sides_li[-self.settings.insist_range_trigger :]
+                chosen_sides_li_slice = self.chosen_sides_li[-self.settings.insist_range_trigger :]
             else:
-                slice = self.chosen_sides_li
-            left_num_chosen = sum(map(lambda x: x == "left", slice))
-            right_num_chosen = sum(map(lambda x: x == "right", slice))
+                chosen_sides_li_slice = self.chosen_sides_li
+            left_num_chosen = sum(map(lambda x: x == "left", chosen_sides_li_slice))
+            right_num_chosen = sum(map(lambda x: x == "right", chosen_sides_li_slice))
             if left_num_chosen >= self.settings.insist_range_trigger:
                 self.insist_mode_active = True
+                print(self.insist_mode_active)
                 self.chosen_sides_li = []
                 self.insist_side = "right"
                 print("\n--------------------------------\n")
                 print("INSIST MODE ACTIVATED: insist right")
-                print("\n--------------------------------")
+                print("\n--------------------------------\n")
+                return
             if right_num_chosen >= self.settings.insist_range_trigger:
                 self.insist_mode_active = True
                 self.chosen_sides_li = []
                 self.insist_side = "left"
                 print("\n--------------------------------\n")
                 print("INSIST MODE ACTIVATED: insist left")
-                print("\n--------------------------------")
+                print("\n--------------------------------\n")
+                return
+            return
         # deactivate insist mode
-        elif self.insist_mode_active:
+        if self.insist_mode_active:
             self.insist_mode_chosen_side_li.append(self.chosen_sides_li[-1])
-            print(self.chosen_sides_li)
-            print(self.insist_mode_chosen_side_li)
-            # get insist mode slice
             if len(self.insist_mode_chosen_side_li) >= self.settings.insist_range_deactivate:
-                slice = self.insist_mode_chosen_side_li[-self.settings.insist_range_deactivate :]
+                chosen_sides_li_slice = self.insist_mode_chosen_side_li[-self.settings.insist_range_deactivate :]
             else:
-                slice = self.insist_mode_chosen_side_li
+                chosen_sides_li_slice = self.insist_mode_chosen_side_li
             # check range if correct
-            insist_correct_choice = sum(map(lambda x: x == self.insist_side, slice))
+            insist_correct_choice = sum(map(lambda x: x == self.insist_side, chosen_sides_li_slice))
             if insist_correct_choice >= self.settings.insist_correct_deactivate:
                 self.insist_mode_active = False
-                self.insist_side = "none"
-                self.insist_mode_chosen_side_li = []  # clear last insist mode list
-                print("\n--------------------------------\n")
+                self.insist_side = None
+                self.insist_mode_chosen_side_li = []
+                print("\n---------------------\n")
                 print("INSIST MODE DEACTIVATED")
-                print("\n--------------------------------")
+                print("\n---------------------\n")
 
     def rule_switch_check(self, current_trial_num):
-        if (
-            current_trial_num >= self.settings.rule_switch_initial_trials_wait
-        ):  # wait initial trials bevore checking for rule switch
-            rule_switch_range = self.trials_correct_side_chosen[
-                self.settings.rule_switch_initial_trials_wait :
-            ]  # only check for rule switch after initial wait
-            if (
-                len(rule_switch_range) >= self.settings.rule_switch_trial_check_range
-            ):  # > not >= because trial counter in main states loop starts from 1 not 0
-                slice = rule_switch_range[-self.settings.rule_switch_trial_check_range :]
-                correct_chosen = sum(slice)  # get number of correct choices
+        if current_trial_num >= self.settings.rule_switch_initial_trials_wait:
+            rule_switch_range = self.trials_correct_side_chosen[self.settings.rule_switch_initial_trials_wait :]
+            # > not >= because trial counter in main states loop starts from 1 not 0
+            if len(rule_switch_range) >= self.settings.rule_switch_trial_check_range:
+                rule_switch_range_slice = rule_switch_range[-self.settings.rule_switch_trial_check_range :]
+                correct_chosen = sum(rule_switch_range_slice)
                 # check if rule switch
                 if self.is_initial_rule_active:
                     if correct_chosen >= self.settings.rule_switch_trials_correct_trigger_switch:
@@ -129,6 +125,6 @@ class ProbabilityConstructor:
                         print("\n switch to rule RU1\n")
                         print("\n--------------------------------\n")
                         # invert stimulus configuration
-                        bk = self.settings.stimulus_correct.copy()
-                        self.settings.stimulus_correct = self.settings.stimulus_wrong.copy()
-                        self.settings.stimulus_wrong = bk
+                        bk = self.settings.stimulus_correct_side.copy()
+                        self.settings.stimulus_correct_side = self.settings.stimulus_wrong_side.copy()
+                        self.settings.stimulus_wrong_side = bk
