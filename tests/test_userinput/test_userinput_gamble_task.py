49f278567e341e1f40282ca471f5acbb07450010
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-19 15:27:34 +0100

    Add REPL to test userinput gamble

--- tests/test_userinput/test_userinput_gamble_task.py
+++ tests/test_userinput/test_userinput_gamble_task.py
@@ -26,41 +26,37 @@ class TestUserInputGambleTask(unittest.TestCase):
         self.usersettings_example_import = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(self.usersettings_example_import)
         self.parameter_handler = TrialParameterHandler(self.usersettings_example_import, "", "")
+        self.window = UserInput(self.parameter_handler)
+        self.window.draw_window_before()
+        self.widget = self.window.root
+        self.widget.update_idletasks()
 
     def tearDown(self):
         self.parameter_handler = None
+        self.window = None
+        self.widget = None
 
     def click_on_widget(self, widget, x, y, button=1):
-        widget.focus_force()
-        widget.event_generate(f"<ButtonPress-{button}>", x=x, y=y)
-        widget.update_idletasks()
+        self.widget.focus_force()
+        self.widget.event_generate(f"<ButtonPress-{button}>", x=x, y=y)
+        self.widget.update_idletasks()
 
     # test helpers
     def block_time_range_tester(self, block_index):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.blocks[block_index].var_range_min.set(NEW_TIME_MIN)
-        window.blocks[block_index].var_range_max.set(NEW_TIME_MAX)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.blocks[block_index].var_range_min.set(NEW_TIME_MIN)
+        self.window.blocks[block_index].var_range_max.set(NEW_TIME_MAX)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         trial_range_block = self.parameter_handler.blocks[block_index]["trial_range_block"]
         self.assertEqual(trial_range_block[0], NEW_TIME_MIN)
         self.assertEqual(trial_range_block[1], NEW_TIME_MAX)
 
     def block_probability_tester(self, block_index):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.blocks[block_index].var_prob_gb.set(NEW_PROBABILITY_GAMBLE)
-        window.blocks[block_index].var_prob_save.set(NEW_PROBABILITY_SAVE)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.blocks[block_index].var_prob_gb.set(NEW_PROBABILITY_GAMBLE)
+        self.window.blocks[block_index].var_prob_save.set(NEW_PROBABILITY_SAVE)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         probability_gamble = self.parameter_handler.blocks[block_index]["prob_reward_gamble_block"]
         probability_save = self.parameter_handler.blocks[block_index]["prob_reward_save_block"]
@@ -68,26 +64,22 @@ class TestUserInputGambleTask(unittest.TestCase):
         self.assertEqual(probability_save, NEW_PROBABILITY_SAVE)
 
     def times_tester(self, time_dict_key: str):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
         all_time_objects = {
-            "time_start": window.time_start,
-            "time_wheel_stopping_check": window.time_wheel_stopping_check,
-            "time_wheel_stopping_punish": window.time_wheel_stopping_punish,
-            "time_stimulus_presentation": window.time_stimulus_presentation,
-            "time_open_loop": window.time_open_loop,
-            "time_stimulus_freeze": window.time_stimulus_freeze,
-            "time_reward": window.time_reward,
-            "time_no_reward": window.time_reward,
-            "time_inter_trial": window.time_inter_trial,
-            "time_open_loop_fail_punish": window.time_open_loop_fail_punish,
+            "time_start": self.window.time_start,
+            "time_wheel_stopping_check": self.window.time_wheel_stopping_check,
+            "time_wheel_stopping_punish": self.window.time_wheel_stopping_punish,
+            "time_stimulus_presentation": self.window.time_stimulus_presentation,
+            "time_open_loop": self.window.time_open_loop,
+            "time_stimulus_freeze": self.window.time_stimulus_freeze,
+            "time_reward": self.window.time_reward,
+            "time_no_reward": self.window.time_reward,
+            "time_inter_trial": self.window.time_inter_trial,
+            "time_open_loop_fail_punish": self.window.time_open_loop_fail_punish,
         }
         time_object = all_time_objects[time_dict_key]
 
         time_object.var.set(NEW_TIME_VALUE)
-        window.on_confirm()
+        self.window.on_confirm()
 
         self.assertEqual(self.parameter_handler.time_dict[time_dict_key], NEW_TIME_VALUE)
 
@@ -96,116 +88,79 @@ class TestUserInputGambleTask(unittest.TestCase):
 
     # tests
     def test_draw_userinput_window(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        window.root.update_idletasks()
-
-        window.on_close()
+        self.window.on_close()
 
     def test_on_confirm(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.on_confirm()
+        self.window.on_confirm()
 
         self.assertTrue(self.parameter_handler.run_session)
 
     def test_on_cancel(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.on_cancel()
+        self.window.on_cancel()
 
         self.assertFalse(self.parameter_handler.run_session)
 
     def test_change_gamble_side(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.var_gamble_side.set(NEW_GAMBLE_SIDE)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.var_gamble_side.set(NEW_GAMBLE_SIDE)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         self.assertEqual(self.parameter_handler.gamble_side, NEW_GAMBLE_SIDE)
 
-    def test_block_1(self):
+    def test_block_1_range(self):
         self.block_time_range_tester(0)
+
+    def test_block_1_probability(self):
         self.block_probability_tester(0)
 
-    def test_block_2(self):
+    def test_block_2_range(self):
         self.block_time_range_tester(1)
+
+    def test_block_2_probability(self):
         self.block_probability_tester(1)
 
-    def test_block_3(self):
+    def test_block_3_range(self):
         self.block_time_range_tester(2)
+
+    def test_block_3_probability(self):
         self.block_probability_tester(2)
 
     # test reward
     def test_big_reward_amount(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.var_big_reward.set(NEW_AMOUNT)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.var_big_reward.set(NEW_AMOUNT)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         self.assertEqual(self.parameter_handler.big_reward, NEW_AMOUNT)
 
     def test_small_reward_amount(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.var_small_reward.set(NEW_AMOUNT)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.var_small_reward.set(NEW_AMOUNT)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         self.assertEqual(self.parameter_handler.small_reward, NEW_AMOUNT)
 
     # test stimulus
     def test_stimulus_radius(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.var_stim_rad.set(NEW_STIMULUS_RADIUS)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.var_stim_rad.set(NEW_STIMULUS_RADIUS)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         self.assertEqual(self.parameter_handler.stimulus_radius, NEW_STIMULUS_RADIUS)
 
     def test_stimulus_color(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
-
-        window.var_stim_col.set(NEW_STIMULUS_COLOR_RBG)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.var_stim_col.set(NEW_STIMULUS_COLOR_RBG)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         rgb_list = self.get_list_from_rgb_string(NEW_STIMULUS_COLOR_RBG)
         self.assertEqual(self.parameter_handler.stimulus_color, rgb_list)
 
     def test_window_background(self):
-        window = UserInput(self.parameter_handler)
-        window.draw_window_before()
-        widget = window.root
-        widget.update_idletasks()
 
-        window.var_background_color.set(NEW_BACKGROUND_COLOR_RBG)
-        widget.update_idletasks()
-        window.on_confirm()
+        self.window.var_background_color.set(NEW_BACKGROUND_COLOR_RBG)
+        self.widget.update_idletasks()
+        self.window.on_confirm()
 
         rgb_list = self.get_list_from_rgb_string(NEW_BACKGROUND_COLOR_RBG)
         self.assertEqual(self.parameter_handler.background_color, rgb_list)
