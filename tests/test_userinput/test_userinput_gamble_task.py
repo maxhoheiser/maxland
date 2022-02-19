1e91bec2124c7db0ca6ea657f47cab14b7fbc607
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-19 13:21:26 +0100

    Add basic unittest for userinput

---
+++ tests/test_userinput/test_userinput_gamble_task.py
@@ -0,0 +1,25 @@
+import importlib.util
+import os
+import unittest
+from pathlib import Path
+
+from maxland.parameter_handler import TrialParameterHandler
+from maxland.userinput import UserInput
+
+USERSETTINGS = os.path.join(Path(os.path.dirname(__file__)).parent.absolute(), "usersettings_example_gamble_task.py")
+
+
+class TestUserInputGambleTask(unittest.TestCase):
+    def setUp(self):
+        spec = importlib.util.spec_from_file_location("usersettings", USERSETTINGS)
+        self.usersettings_example_import = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(self.usersettings_example_import)
+        self.parameter_handler = TrialParameterHandler(self.usersettings_example_import, "", "")
+
+    def tearDown(self):
+        self.parameter_handler = None
+
+    def test_draw_userinput_window(self):
+        window = UserInput(self.parameter_handler)
+        window.draw_window_before()
+        window.on_close()
