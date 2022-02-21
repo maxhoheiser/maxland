18fdabb5abc32a2adb4ed676de295fc2cb111014
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-02-22 00:21:40 +0100

    Add threading and all tests for stimulus confidentiality

--- tests/test_stimulus/hw_test_stimulus_confidentiality_task.py
+++ tests/test_stimulus/hw_test_stimulus_confidentiality_task.py
@@ -1,10 +1,14 @@
 import importlib.util
 import os
+import threading
+import time
 import unittest
 from pathlib import Path
+from unittest.mock import MagicMock
 
 from maxland.parameter_handler import TrialParameterHandler
 from maxland.probability_conf import ProbabilityConstructor
+from maxland.stimulus_conf import Stimulus
 
 USERSETTINGS = os.path.join(Path(os.path.dirname(__file__)).parent.absolute(), "usersettings_example_conf_task.py")
 CORRECT_SIDE_LEFT = "left"
@@ -34,3 +38,143 @@ class TestStimulusGambleTask(unittest.TestCase):
     def tearDown(self):
         self.parameter_handler = None
         self.probability_constructor = None
+
+    def thread_function(self, game):
+        time.sleep(2)
+        game.stop_closed_loop_before()
+        time.sleep(2)
+        game.stop_open_loop()
+
+    def test_load_stimulus_conf(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = []
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+        time.sleep(1)
+        game.on_close()
+
+    def test_run_game_3(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = [["a", "b", 10]]
+        event_flag = MagicMock()
+        event_flag.wait.return_value = time.sleep(2)
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+
+        event_flags = {
+            "event_display_stimulus": event_flag,
+            "event_still_show_stimulus": event_flag,
+        }
+
+        thread = threading.Thread(target=self.thread_function, args=(game,))
+
+        thread.start()
+        game.run_game_3(event_flags)
+
+        thread.join()
+        game.on_close()
+
+    def test_run_game_2(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = [["a", "b", 10]]
+        event_flag = MagicMock()
+        event_flag.wait.return_value = time.sleep(2)
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+
+        event_flags = {
+            "event_display_stimulus": event_flag,
+            "event_still_show_stimulus": event_flag,
+        }
+
+        thread = threading.Thread(target=self.thread_function, args=(game,))
+
+        thread.start()
+        game.run_game_2(event_flags)
+
+        thread.join()
+        game.on_close()
+
+    def test_run_game_1(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = [["a", "b", 10]]
+        event_flag = MagicMock()
+        event_flag.wait.return_value = time.sleep(2)
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+
+        event_flags = {
+            "event_display_stimulus": event_flag,
+            "event_still_show_stimulus": event_flag,
+        }
+
+        thread = threading.Thread(target=self.thread_function, args=(game,))
+
+        thread.start()
+        game.run_game_1(event_flags)
+
+        thread.join()
+        game.on_close()
+
+    def test_run_game_habituation_3_simple(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = [["a", "b", 10]]
+        event_flag = MagicMock()
+        event_flag.wait.return_value = time.sleep(2)
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+
+        event_flags = {
+            "event_display_stimulus": event_flag,
+            "event_still_show_stimulus": event_flag,
+        }
+
+        thread = threading.Thread(target=self.thread_function, args=(game,))
+
+        thread.start()
+        game.run_game_habituation_3_simple(event_flags)
+
+        thread.join()
+        game.on_close()
+
+    def test_run_game_habituation_3_complex(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = [["a", "b", 10]]
+        event_flag = MagicMock()
+        event_flag.wait.return_value = time.sleep(2)
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+
+        event_flags = {
+            "event_display_stimulus": event_flag,
+            "event_still_show_stimulus": event_flag,
+        }
+
+        thread = threading.Thread(target=self.thread_function, args=(game,))
+
+        thread.start()
+        game.run_game_habituation_3_complex(event_flags)
+
+        thread.join()
+        game.on_close()
+
+    def test_run_game_habituation_2(self):
+        rotary_encoder = MagicMock()
+        rotary_encoder.rotary_encoder.read_stream.return_value = [["a", "b", 10]]
+        event_flag = MagicMock()
+        event_flag.wait.return_value = time.sleep(2)
+
+        game = Stimulus(self.parameter_handler, rotary_encoder, CORRECT_SIDE_LEFT)
+
+        event_flags = {
+            "event_display_stimulus": event_flag,
+            "event_still_show_stimulus": event_flag,
+        }
+
+        thread = threading.Thread(target=self.thread_function, args=(game,))
+
+        thread.start()
+        game.run_game_habituation_2(event_flags)
+
+        thread.join()
+        game.on_close()
