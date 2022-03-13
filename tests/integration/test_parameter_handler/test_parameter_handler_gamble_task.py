c6b5bfe2a00fe822b117efe376c2c362a4c83206
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-03-13 16:47:06 +0100

    Add test all usersettings variables in parameter handler test

--- tests/integration/test_parameter_handler/test_parameter_handler_gamble_task.py
+++ tests/integration/test_parameter_handler/test_parameter_handler_gamble_task.py
@@ -6,12 +6,45 @@ import unittest
 from pathlib import Path
 
 from maxland.parameter_handler import TrialParameterHandler
+from maxland.types_time_dict import TimeDict
+from maxland.types_usersettings import GambleSide, TaskName
 
 USERSETTINGS_EXAMPLE_GAMBLE_TASK = os.path.join(
     Path(os.path.dirname(__file__)).parent.absolute().parent.absolute(), "usersettings_example_gamble_task.py"
 )
+
+# Mock usersettings data
+NEW_TASK = TaskName.GAMBLE
+NEW_GAMBLE_SIDE = GambleSide.RIGHT
+NEW_BLOCKS = [
+    {"trial_range_block": [3, 4], "prob_reward_gamble_block": 11.0, "prob_reward_save_block": 21.0},
+    {"trial_range_block": [2, 3], "prob_reward_gamble_block": 31.0, "prob_reward_save_block": 41.0},
+    {"trial_range_block": [5, 4], "prob_reward_gamble_block": 51.0, "prob_reward_save_block": 51.0},
+]
+# reward in seconds
 NEW_BIG_REWARD = 10.15
 NEW_SMALL_REWARD = 11.16
+NEW_LAST_CALLIBRATION = "2020.06.10"
+# trial times
+NEW_TIME_START = 3.0
+NEW_TIME_WHEEL_STOPPING_CHECK = 2.0
+NEW_TIME_WHEEL_STOPPING_PUNISH = 2.0
+NEW_TIME_PRESENT_STIMULUS = 2.0
+NEW_TIME_OPEN_LOOP = 10.1
+NEW_TIME_OPEN_LOOP_FAIL_PUNISH = 1.0
+NEW_TIME_STIMULUS_FREEZE = 2.3
+NEW_TIME_REWARD = 1.6
+NEW_TIME_NO_REWARD = 2.4
+TIME_INTER_TRIAL = 3
+# stimulus
+NEW_STIMULUS_RADIUS = 46
+NEW_STIMULUS_COLOR = [1, 252, 1]
+NEW_BACKGROUND_COLOR = [1, 2, 3]
+# thresholds
+NEW_ROTARYENCODER_THRESHOLDS = [-91, 92, -3, 4]
+NEW_STIMULUS_END_POSITION = [-2047, 2049]
+NEW_LIFE_PLOT = False
+NEW_ANIMAL_WEIGHT = 11
 
 
 def get_test_folder_paths(file_path):
@@ -56,8 +89,35 @@ class TestTrialParameterHandlerGambleTask(unittest.TestCase):
         usersettings_object_before = TrialParameterHandler(
             self.usersettings_example_import, self.settings_folder_path, self.session_folder_path
         )
+        usersettings_object_before.task_name = NEW_TASK
+        usersettings_object_before.gamble_side = NEW_GAMBLE_SIDE
+        usersettings_object_before.blocks = NEW_BLOCKS
         usersettings_object_before.big_reward = NEW_BIG_REWARD
         usersettings_object_before.small_reward = NEW_SMALL_REWARD
+        usersettings_object_before.last_callibration = NEW_LAST_CALLIBRATION
+
+        new_time_dict: TimeDict = {
+            "time_start": NEW_TIME_START,
+            "time_wheel_stopping_check": NEW_TIME_WHEEL_STOPPING_CHECK,
+            "time_wheel_stopping_punish": NEW_TIME_WHEEL_STOPPING_PUNISH,
+            "time_present_stimulus": NEW_TIME_PRESENT_STIMULUS,
+            "time_open_loop": NEW_TIME_OPEN_LOOP,
+            "time_open_loop_fail_punish": NEW_TIME_OPEN_LOOP_FAIL_PUNISH,
+            "time_stimulus_freeze": NEW_TIME_STIMULUS_FREEZE,
+            "time_reward": NEW_TIME_REWARD,
+            "time_no_reward": NEW_TIME_NO_REWARD,
+            "time_inter_trial": TIME_INTER_TRIAL,
+        }
+        usersettings_object_before.time_dict = new_time_dict
+
+        usersettings_object_before.stimulus_radius = NEW_STIMULUS_RADIUS
+        usersettings_object_before.stimulus_color = NEW_STIMULUS_COLOR
+        usersettings_object_before.background_color = NEW_BACKGROUND_COLOR
+        usersettings_object_before.rotaryencoder_thresholds = NEW_ROTARYENCODER_THRESHOLDS
+        usersettings_object_before.stimulus_end_position = NEW_STIMULUS_END_POSITION
+        usersettings_object_before.life_plot = NEW_LIFE_PLOT
+        usersettings_object_before.animal_weight = NEW_ANIMAL_WEIGHT
+
         usersettings_object_before.update_userinput_file_gamble()
 
         import_file = os.path.join(self.settings_folder_path, "usersettings.py")
@@ -68,8 +128,27 @@ class TestTrialParameterHandlerGambleTask(unittest.TestCase):
             usersettings_example_import_after, self.settings_folder_path, self.session_folder_path
         )
 
+        self.assertEqual(usersettings_object_after.task_name, NEW_TASK)
+        self.assertEqual(usersettings_object_after.gamble_side, NEW_GAMBLE_SIDE)
+        self.assertEqual(usersettings_object_after.blocks, NEW_BLOCKS)
         self.assertEqual(usersettings_object_after.big_reward, NEW_BIG_REWARD)
         self.assertEqual(usersettings_object_after.small_reward, NEW_SMALL_REWARD)
+        self.assertEqual(usersettings_object_after.last_callibration, NEW_LAST_CALLIBRATION)
+
+        for key, item in new_time_dict.items():
+            self.assertEqual(usersettings_object_after.time_dict[key], item)
+
+        self.assertEqual(usersettings_object_after.stimulus_radius, NEW_STIMULUS_RADIUS)
+        self.assertEqual(usersettings_object_after.stimulus_color, NEW_STIMULUS_COLOR)
+        self.assertEqual(usersettings_object_after.background_color, NEW_BACKGROUND_COLOR)
+        self.assertEqual(usersettings_object_after.rotaryencoder_thresholds, NEW_ROTARYENCODER_THRESHOLDS)
+        self.assertEqual(usersettings_object_after.stimulus_end_position, NEW_STIMULUS_END_POSITION)
+        self.assertEqual(usersettings_object_after.life_plot, NEW_LIFE_PLOT)
+        self.assertEqual(usersettings_object_after.animal_weight, NEW_ANIMAL_WEIGHT)
+
+        self.assertEqual(usersettings_object_after.big_reward, NEW_BIG_REWARD)
+        self.assertEqual(usersettings_object_after.small_reward, NEW_SMALL_REWARD)
+        self.assertEqual(usersettings_object_after.gamble_side, NEW_GAMBLE_SIDE)
 
     def test_save_complete_usersettings_to_file(self):
         usersettings_object_before = TrialParameterHandler(
