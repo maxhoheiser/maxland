d0aad2c81b37941434fab420398729591d3f9b51
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-03-15 11:19:14 +0100

    Add rules for parameter handler training complex

---
+++ tests/usersettings_example_conf_task_complex.py
@@ -0,0 +1,72 @@
+TASK = "conf"
+STAGE = "training-complex"
+
+TRIAL_NUMBER = 30
+STIMULUS_TYPE = "two-stimuli"  # three-stimuli #two-stimuli #one-stimulus
+GRATING_SIZE = 100.0
+GRATING_SPEED = 0.04
+
+RULE_A = {
+    "a00b03": {"correct": True, "conflicting": False},
+    "a00b02": {"correct": True, "conflicting": False},
+    "a00b01": {"correct": True, "conflicting": False},
+    "a00b00": {"correct": True, "conflicting": False},
+    "a03b03": {"correct": False, "conflicting": False},
+    "a03b02": {"correct": False, "conflicting": False},
+    "a03b01": {"correct": False, "conflicting": False},
+    "a03b00": {"correct": False, "conflicting": False},
+}
+
+RULE_B = {
+    "a00b03": {"correct": False, "conflicting": False},
+    "a00b02": {"correct": False, "conflicting": False},
+    "a00b01": {"correct": False, "conflicting": False},
+    "a00b00": {"correct": False, "conflicting": False},
+    "a03b03": {"correct": True, "conflicting": False},
+    "a03b02": {"correct": True, "conflicting": False},
+    "a03b01": {"correct": True, "conflicting": False},
+    "a03b00": {"correct": True, "conflicting": False},
+}
+
+# reward in seconds
+REWARD = 0.12
+LAST_CALLIBRATION = "2020.06.10"
+
+# trial times
+TIME_START = 1.0
+TIME_WHEEL_STOPPING_CHECK = 1.0
+TIME_WHEEL_STOPPING_PUNISH = 0.0
+TIME_PRESENT_STIMULUS = 1.0
+TIME_OPEN_LOOP = 10.0
+TIME_OPEN_LOOP_FAIL_PUNISH = 0.0
+TIME_STIMULUS_FREEZE = 2.0
+TIME_REWARD = 1.0
+TIME_RANGE_NO_REWARD_PUNISH = [0.0, 0.0]
+TIME_INTER_TRIAL = 1.5
+
+# insist mode
+INSIST_RANGE_TRIGGER = 5
+INSIST_CORRECT_DEACTIVATE = 3
+INSIST_RANGE_DEACTIVATE = 35
+
+# rule switching
+RULE_SWITCH_INITIAL_TRIALS_WAIT = 10
+RULE_SWITCH_CHECK_TRIAL_RANGE = 10
+RULE_SWITCH_TRIALS_CORRECT_TRIGGER_SWITCH = 8
+
+# fade away
+FADE_START = 1950
+FADE_END = 3000
+
+# stimulus size and color - only for moving stimulus
+STIMULUS_RADIUS = 45  # pixel radius of stimulus
+STIMULUS_COLOR = [0, 255, 0]  # color of stimulus
+BACKGROUND_COLOR = [0, 0, 0]
+
+# thresholds
+ROTARYENCODER_THRESHOLDS = [-90, 90, -2, 2]
+STIMULUS_END_POSITION = [-400, 400]  # pixel
+
+LIFE_PLOT = False
+# animal weight in grams
+ANIMAL_WEIGHT = 10.0
