9adf7e81b957a86f908e0b2fb20799b7e026b18d
Maximilian Hoheiser <max.hoheiser@gmail.com>
2022-05-03 13:04:51 +0200

    Add task files

---
+++ tasks/confidentiality_task/confidentiality_task_habituation_complex_rule_based_both_correct/rule_definition.py
@@ -0,0 +1,41 @@
+RULE_A = [
+    {
+        "correct": "a00b03",
+        "wrong": "a03b00",
+        "conflicting": True,
+        "percentage": 0.4,
+    },
+    {
+        "correct": "a00b02",
+        "wrong": "a03b02",
+        "conflicting": True,
+        "percentage": 0.3,
+    },
+    {
+        "correct": "a00b01",
+        "wrong": "a03b03",
+        "conflicting": True,
+        "percentage": 0.3,
+    },
+]
+
+RULE_B = [
+    {
+        "correct": "a01b01",
+        "wrong": "a02b01",
+        "conflicting": True,
+        "percentage": 0.4,
+    },
+    {
+        "correct": "a01b02",
+        "wrong": "a02b02",
+        "conflicting": True,
+        "percentage": 0.3,
+    },
+    {
+        "correct": "a01b03",
+        "wrong": "a02b03",
+        "conflicting": True,
+        "percentage": 0.3,
+    },
+]
